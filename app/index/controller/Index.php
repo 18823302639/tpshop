c1bfce99c4535fba561387da325758db11362bee
xuan <xuan>
2019-01-17 18:25:05 +0800

    短信验证码成

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -3,6 +3,8 @@ namespace app\index\controller;
 use \think\Controller;
 use \think\Request;
 use app\admin\Controller\Uploads;
+//引入云之讯短信类
+use \sms\UcpaasClass;
 
 class Index extends Controller
 {
@@ -67,5 +69,31 @@ class Index extends Controller
         $column_id = $_GET['column_id'];
 
         $this->fetch('column');
+    }
+
+    public function sms(){
+        if(request()->isPost()){
+            //初始化必填
+            //填写在开发者控制台首页上的Account Sid
+            $options['accountsid'] = '08e9739a5c725147a91c8c180e24c277';
+            //填写在开发者控制台首页上的Auth Token
+            $options['token'] = '91fb59c67b4f90c58dfd19ef15b84020';
+
+            //初始化 $options必填
+            $ucpass = new \sms\UcpaasClass($options);
+
+            $appid = "26bca8343ca34783a0213d20f8c100d1";	//应用的ID，可在开发者控制台内的短信产品下查看
+            $templateid = "395397";    //可在后台短信产品→选择接入的应用→短信模板-模板ID，查看该模板ID
+            $param = $_POST['yzm']; //多个参数使用英文逗号隔开（如：param=“a,b,c”），如为参数则留空
+            $mobile = $_POST['yzmtel'];
+            $uid = "";
+
+            //70字内（含70字）计一条，超过70字，按67字/条计费，超过长度短信平台将会自动分割为多条发送。分割后的多条短信将按照具体占用条数计费。
+
+            echo $ucpass->SendSms($appid,$templateid,$param,$mobile,$uid);
+
+
+        }
+        return view("sms/index");
     }
 }
